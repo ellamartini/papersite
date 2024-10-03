7e13a70c816b764d65d1e4c519b89f2b376e285b
ellamartini <144989040+ellamartini@users.noreply.github.com>
2024-10-02 18:49:33 -0700

    Add files via upload

--- home.js
+++ home.js
@@ -1,16 +1,10 @@
-function setup() {
-    canvas = createCanvas(windowWidth,windowHeight);
-    canvas.parent("sketch-container");
+const musclyMen = document.getElementById('muscle');
 
-    // fill("lightblue");
-    // rectMode(CENTER);
-    // rect(500, 200, 600, 400);
-    // fill("black")
-    // textSize(15);
-    // text("Yakuza, or Like A Dragon depending on where you are and who you ask, is a narrative-driven Japanese video game franchise about organized crime where huge, muscly men take their shirts off and punch each other until they enter “climax heat mode”. Despite what this summary might suggest, since Yakuza first released in 2005, the studio has openly targeted a young, straight male audience. Series founder Toshihiro Nagoshi shared in 2019 that women made up about 20% of their overall player base. Responding to a small uptick this demographic, Nagoshi said, “Yakuza is something that is made for male players, so we will be careful not to be too conscious of the female users and derail from what we want to make.” Although these numbers have likely shifted somewhat since the release of their global hits Yakuza: Like A Dragon in 2020 and Like A Dragon: Infinite Wealth in 2024, the studio’s emphasis on male-centered stories and perspectives puts these most recent titles in alignment with the franchise’s long-standing focus on male audiences.", windowWidth/2, (windowHeight/2)-100, 570)
+musclyMen.addEventListener('mouseover', () => {
+    document.body.style.backgroundImage = "url('assets/kiryu_nishiki.jpg')";
+    // document.body.style.color = 'black';
+})
 
-  }
-  
-  function draw() {
-
-  }
+musclyMen.addEventListener('mouseout', () => {
+    document.body.style.backgroundImage = "url('assets/kamurocho_street_night.jpg')"
+})
